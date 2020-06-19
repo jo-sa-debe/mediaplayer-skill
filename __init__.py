400bdbf61ae5fc42c380d6dc6d81a6eee2450f4f
jo-sa-debe <sauwenjohan@hotmail.com>
2020-06-19 18:04:10 +0200

    init

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ class Mediaplayer(MycroftSkill):
         self.audio_service = AudioService(self.bus)
 
 
-    @intent_file_handler('mediaplayer.stop.intent')
+    @intent_file_handler('mediaplayer.info.intent')
     def handle_mediaplayer(self, message):
         self.speak_dialog('mediaplayer')
         self.speak("looking for backends.")
