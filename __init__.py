df8aca3a470ec50276844d594e87fbd9ef2c82e4
jo-sa-debe <sauwenjohan@hotmail.com>
2020-06-19 23:58:05 +0200

    init

--- __init__.py
+++ __init__.py
@@ -61,8 +61,8 @@ class Mediaplayer(MycroftSkill):
 
     def play(self, message):
         self.speak("Start Playing")
-        for track in self.vlc_all_tracks:
-            self.speak('track : ' + track.index(0))
+        for track in self.vlc_all_tracks[0]:
+            self.speak('track : ' + track )
         self.speak("end of tracklist : all")
         self.audio_service.play(self.vlc_all_tracks)
 
